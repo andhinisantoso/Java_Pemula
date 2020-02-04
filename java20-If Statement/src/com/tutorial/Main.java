5eaa0ef00e771254c710f30ba1ebd915c2e34c90
Andhini Rahma Santoso <45195243+andhinisantoso@users.noreply.github.com>
2020-02-04 16:05:25 +0700

    If Statements
    Percabangan if pada Java

---
+++ java20-If Statement/src/com/tutorial/Main.java
@@ -0,0 +1,24 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // tutorial untuk percabangan
+        int a = 5;
+        System.out.println("nilai = " + a);
+        
+        //percabangan if
+        if (a==3){
+            System.out.println("Ini adalah cabang");
+        }else {
+            System.out.println("Ini jalur default");
+        }
+        
+        System.out.println("selesai");
+        
+        
+        
+        
+    }
+    
+}
