a37ee590adf7d6627f6050edc165663e9f92da54
Andhini Rahma Santoso <45195243+andhinisantoso@users.noreply.github.com>
2020-02-04 16:07:04 +0700

    If Else If Statement
    Pemprograman percabanagan If else If

---
+++ java21-If Else If Statement/src/com/tutorial/Main.java
@@ -0,0 +1,25 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // percabangan if else if
+        
+        int a = 10;
+        
+        System.out.println("Ini awal program");
+        
+        // awal percabangan
+        if ( a==10 ){
+            System.out.println("Ini adalah statement1");
+        }else if( a== 10 ){
+            System.out.println("Ini adalah statement2");
+        }else{
+            System.out.println("Ini default");
+        }
+        
+        // akhir percabangan
+        
+        System.out.println("Ini adalah akhir program");
+    }
+}
